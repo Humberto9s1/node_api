386f98dad23eec74d1269919a80616ea8c040492
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-03-13 08:14:28 -0300

    npm install express

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send("<html><body>Sistema de Locação</body></html>");
+});
+
+app.get('/equipments', function(req, res){
+    res.send("<html><body>Cadastro de Equipamentos</body></html>");
+});
+
+app.listen(3000, function(){
+
+    console.log("Servidor Rodando com Sucesso");
+
+});
