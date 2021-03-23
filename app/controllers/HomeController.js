f82aeb10052b08b9dffa9929d22619a47681406d
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-03-23 18:39:28 -0300

    Criação de Controller

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -1,3 +1,10 @@
 module.exports.dashboards = function (app, req, res) {
-    res.render("dashboards/createDas");
+    var connection = app.config.dbConnection();
+    var CompaniesModel = new app.app.models.CompaniesModel(connection);
+
+    CompaniesModel.get5UltimasCompanies(function(error, result){
+        console.log(result);
+        res.render("dashboards/createDas",{companies : result});
+    });
+    
 }
--- app/models/CompaniesModel.js
+++ app/models/CompaniesModel.js
@@ -14,6 +14,10 @@ CompaniesModel.prototype.companyStore = function(company, callback) {
     this._connection.query('insert into companies set ?', company, callback);
 };
 
+CompaniesModel.prototype.get5UltimasCompanies = function(companies, callback) {
+    this._connection.query('select * from companies order by id desc limit 5 ', companies, callback);
+};
+
 module.exports = function() {
 
     return CompaniesModel;
